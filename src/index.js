e2d82c99515d191d1e61c56c78ca72e010a5e7d9
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-06-14 20:14:20 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+function displayTemperature(response) {
+	cityElement.innerHTML = response.data.city;
+}
+
 function search(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector('#search-input');
 	let cityElement = document.querySelector('#city');
 	cityElement.innerHTML = searchInput.value;
 	searchCity(searchInput.value);
+
+	if (searchInput) {
+		document.querySelector('#search-form').textContent = searchInput;
+	}
 }
 
 let searchForm = document.querySelector('#search-input');
-searchForm.addEventListener('submit', search)
+searchForm.addEventListener('submit', search);
